128293cd8ce424bb0d6e5f9bfb67727a59735a56
Ashley Norton <nortonashleym@gmail.com>
2020-03-25 03:58:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     time = Time.now.to_i
     
-    if time < 12 
+    if time < 12:00 
       resp.write "Good Morning!"
     else 
       resp.write "Good Afternoon!"
