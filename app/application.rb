8d03a7b4374da4ca9fb5c4a68b1b20685f76a6b2
Ashley Norton <nortonashleym@gmail.com>
2020-03-25 03:58:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,14 @@
 class Application
+  
   def call(env)
     resp = Rack::Response.new
     time = Time.now.to_i
     
     if time < 12 
-      resp.write ""
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
+    end 
     
     resp.finish
   end
